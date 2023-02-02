abfe4a07d0f2946025d1bf366ae13fe7c0b887e8
s_uja <dge03033@gmail.com>
2023-02-02 21:51:49 +0900

    #10 Navigation Menu 커스텀 편 (19:44)

---
+++ app/src/main/java/com/example/customnaviexample/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.customnaviexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.drawer);
+
+        Button btn_open = (Button) findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
+        Button btn_close = (Button)findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawers();
+                //drawers!!!s가 중요함.
+            }
+        });
+
+    }
+//특정액션시 추가기능을 만들고싶을때...
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+            //슬라이드했을때
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+            //오픈이 완료됏을때
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+            //슬라이드메뉴가닫혔을때
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            //어떤상태가 체인지됐을때
+        }
+    };
+}
